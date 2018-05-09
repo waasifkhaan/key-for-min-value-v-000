2d9fd8bb90b381dbf4296da401e8acbd6d492b32
Wasif Khan <waasif.khaan@gmail.com>
2018-05-09 03:38:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,7 @@ def key_for_min_value(name_hash)
   key1 = nil
   value1 = nil
   name_hash.each do |key , value|
-    if value1.nil?
-      value1 = value
-      key1 = key
-    elsif value < value1
+    if value1.nil? || value < value1
       value1 = value
       key1 = key
     end
