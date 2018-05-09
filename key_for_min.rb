e2a976519304a6e2a28866ab77152692ef53ef8d
Wasif Khan <waasif.khaan@gmail.com>
2018-05-09 02:58:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
   else
     array = []
   name_hash.each do |key , value|
-      array << value  
+      array << value 
+      aray  << key
   end
   array.min
   end
